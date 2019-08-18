9be2d5a8a49aabaf804c3169e571df31c858473c
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-18 23:29:55 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,8 +6,9 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if 
+    if age <=12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end 
   end
 end
 
